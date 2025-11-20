c988299a5f28dadfa840e0778d355373b88063f1
Fredff1 <fuzhice618227@gmail.com>
2025-11-20 14:11:46 +0800

    fix:now val_before_train can be correctly used in training

--- src/verl/trainer/ppo/ray_trainer.py
+++ src/verl/trainer/ppo/ray_trainer.py
@@ -1436,7 +1436,7 @@ class RayPPOTrainer:
 
         # load checkpoint before doing anything
         self._load_checkpoint()
-
+        self._init_wrapper()
         # perform validation before training
         # currently, we only support validation using the reward_function.
         if self.val_reward_fn is not None and self.config.trainer.get("val_before_train", True):
@@ -1455,7 +1455,7 @@ class RayPPOTrainer:
         last_val_metrics = None
         self.max_steps_duration = 0
         
-        self._init_wrapper()
+
         
         for epoch in range(self.config.trainer.total_epochs):
             for batch_dict in self.train_dataloader:
